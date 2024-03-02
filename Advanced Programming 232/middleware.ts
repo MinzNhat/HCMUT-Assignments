9441fd2f4a5e2ed204ad6e1d5751dacdf5484add
Nhat Dang Minh <141141809+MinzNhat@users.noreply.github.com>
2024-03-03 00:12:44 +0700

    Update middleware.ts

--- Advanced Programming 232/middleware.ts
+++ Advanced Programming 232/middleware.ts
@@ -1,10 +1,10 @@
+import { NextResponse } from 'next/server';
+
 export async function middleware(request: any) {
-    if (request.url === '/') {
-        return new Response(null, {
-            status: 302,
-            headers: {
-                'Location': '/data'
-            }
-        });
+    if (request.nextUrl.pathname === '/') {
+        const redirectUrl = new URL('/data', request.url);
+        return NextResponse.redirect(redirectUrl);
     }
+
+    return NextResponse.next();
 }
