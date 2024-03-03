274ffefd7ddaa4113e1cbbea24541701f51248c8
Nhat Dang Minh <141141809+MinzNhat@users.noreply.github.com>
2024-03-03 09:55:27 +0700

    HCMUT

--- Advanced Programming 232/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { rewrite } from '@vercel/edge';
- 
-export default function middleware(request: Request) {
-  const url = new URL(request.url);
- 
-  if (url.pathname.startsWith('/')) {
-    return rewrite(new URL('/data', request.url));
-  }
-}
